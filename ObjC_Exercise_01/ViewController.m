3a8a6a482657aa7071dcbb56e79d5c680d8a02ae
longhoang2984 <longhoang.2984@gmail.com>
2016-03-31 11:57:10 +0700

    Update ViewController.m

--- ObjC_Exercise_01/ViewController.m
+++ ObjC_Exercise_01/ViewController.m
@@ -23,5 +23,27 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)askQuestion:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Question" message:@"2 + 2 * 2 = 8?" preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *yes = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        UIAlertController *result = [UIAlertController alertControllerWithTitle:@"Answer" message:@"That is incorrect" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Done" style:UIAlertActionStyleCancel handler:nil];
+        [result addAction:ok];
+        [self presentViewController:result animated:YES completion:nil];
+        
+        
+    }];
+    
+    UIAlertAction *no = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        UIAlertController *result = [UIAlertController alertControllerWithTitle:@"Answer" message:@"That is correct!" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok = [UIAlertAction actionWithTitle:@"Done" style:UIAlertActionStyleCancel handler:nil];
+        [result addAction:ok];
+        [self presentViewController:result animated:YES completion:nil];
+    }];
+    [alert addAction:yes];
+    [alert addAction:no];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
